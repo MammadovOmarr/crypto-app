e7199fe5cf753ba3c34987fab049752db449fc89
MammadovOmarr <150725849+MammadovOmarr@users.noreply.github.com>
2024-11-20 14:05:24 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, jsonify
 import requests
 from datetime import datetime
+import os
+from urllib.parse import quote as url_quote
+
+
+port = int(os.environ.get("PORT",5000))
 
 app = Flask(__name__)
 
@@ -67,4 +72,4 @@ def crypto_price_page():
     return render_template("crypto_price.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0", port=port)
